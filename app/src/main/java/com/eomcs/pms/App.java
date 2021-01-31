e5a4df8862f4ccbe7a975bafc77c0ff55aba195c
js988174 <rudtjs4902@naver.com>
2021-01-31 17:08:45 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -6,42 +6,14 @@ import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
 import com.eomcs.util.Prompt;
 
-
-// 1) Prompt 클래스를 별도의 패키지로 분류한다
-//    - com.eomcs.util 패키지 생성
-//    - Prompt 클래스를 이 패키지로 옮긴다.
-//    - Prompt 클래스를 다른 패키지의 클래스가 사용할 수 있도록 public 으로 공개한다.
-//    - Prompt의 메서드를 다른 패키지의 클래스가 사용할 수 있도록 public 으로 공개한다.
-// 2) 핸들러 클래스들을 별도의 패키지로 분류한다
-//    - com.eomcs.pms.handler 패키지 생성
-//    - XxxHandler 클래스를 이 패키지로 옮긴다.
-
-//    - 핸들러 클래스를 다른 패키지의 클래스가 사용할 수 있도록 public 으로 공개한다.
-//    - 핸들러의 메서드를 다른 패키지의 클래스가 사용할 수 있도록 public 으로 공개한다.
-
-// 1) '/board/add' 명령 처리 추가
-// 2) '/board/list' 명령 처리 추가
-// 3) 두 번째 게시판 추가
-// 4) 세 번째, 네 번째, 다섯 번째, 여섯 번째 게시판 추가
-
-public class App{
+public class App {
 
   public static void main(String[] args) {
 
-    BoardHandler boardlist = new BoardHandler();
-    // 각 게시판 데이터를 저장할 메모리 준
-
-    // 각 회원 목록 데이터를 저장할 메모리 준비
+    BoardHandler boardList = new BoardHandler();
     MemberHandler memberList = new MemberHandler();
-    // 각 프로젝트 목록 데이터를 저장할 메모리 준비
-    // 생성자에서 MemberHandler 객체를 주입하라고 강요한다.
-    // ProjectHandler 객체를 만들려면 반드시 주입해야 한다.
     ProjectHandler projectList = new ProjectHandler(memberList);
-    projectList.memberList = memberList;
-    // 각 작업 목록 데이터를 저장할 메모리 준비
     TaskHandler taskList = new TaskHandler(memberList);
-
-
 
 
     loop:
@@ -55,11 +27,29 @@ public class App{
           case "/member/list":
             memberList.list();
             break;
+          case "/member/ detail":
+            memberList.detail();
+            break;  
+          case "/member/update":
+            memberList.update();
+            break; 
+          case "/member/delete":
+            memberList.delete();
+            break;
           case "/project/add":
             projectList.add();
             break;
           case "/project/list":
             projectList.list();
+            break;
+          case "/project/detail": 
+            projectList.detail();
+            break;  
+          case "/project/update":
+            projectList.update();
+            break; 
+          case "/project/delete":
+            projectList.delete();
             break;
           case "/task/add":
             taskList.add();
@@ -67,21 +57,30 @@ public class App{
           case "/task/list":
             taskList.list();
             break;
+          case "/task/detail": 
+            taskList.detail();
+            break;  
+          case "/task/update":
+            taskList.update();
+            break; 
+          case "/task/delete":
+            taskList.delete();
+            break;
           case "/board/add":
-            boardlist.add(boardlist);
-            break;        
+            boardList.add();
+            break;
           case "/board/list":
-            boardlist.list(boardlist);
+            boardList.list();
             break;
           case "/board/detail":
-            boardlist.detail();
-            break;       
+            boardList.detail();
+            break;  
           case "/board/update":
-            boardlist.update();
-            break;       
+            boardList.update();
+            break; 
           case "/board/delete":
-            boardlist.delete();
-            break;       
+            boardList.delete();
+            break;
           case "quit":
           case "exit":
             System.out.println("안녕!");
--- app/src/main/java/com/eomcs/pms/handler/BoardHandler.java
+++ app/src/main/java/com/eomcs/pms/handler/BoardHandler.java
@@ -7,15 +7,13 @@ import com.eomcs.util.Prompt;
 public class BoardHandler {
 
 
-  // 공동으로 사용하는 값은 스택틱 필드로 선언한다.
-  static final int LENGTH = 100;
-
-  // 개별적으로 관리하는 값은 인스턴스 필드로 선언헌다.
-  Board[] boards = new Board[LENGTH];   
+  node head;
+  node lastBox;
   int size = 0;
 
 
-  public void add(BoardHandler boardList) {
+
+  public void add() {
     System.out.println("[게시글 등록]");
 
     Board b = new Board();
@@ -26,21 +24,33 @@ public class BoardHandler {
     b.writer = Prompt.inputString("작성자? ");
     b.registeredDate = new Date(System.currentTimeMillis());
 
-    this.boards[this.size++] = b;
+    node box = new node(b);
+
+    if (lastBox == null) { // 연결 리스트의 첫 항목이라면,
+      lastBox.next = box; 
+
+      head = box;   
+
+    } else { // 연결리스트에 이미 항목이 있다면,
+      lastBox.next = box;// 마지막 상자가 다음 상자를 새 상자를 가르키게 한다.
+      box.prev = lastBox;// 새 상자의 이전 상자로서 현재 마지막 상자를 가르키게 한다.
+      lastBox = box; // 새 상자가 마지막 상자가 되게 한다.
+    }
 
     System.out.println("게시글을 등록하였습니다.");
   }
 
-  public void list(BoardHandler boardList) {
+  public void list() {
     System.out.println("[게시글 목록]");
 
-    for (int i = 0; i < boardList.size; i++) {
-      Board b = this.boards[i];
+    node cursor = head;
 
-      if (b == null) 
-        continue;
 
-      // 번호, 제목, 등록일, 작성자, 조회수
+    while (cursor != null) {
+      Board b = cursor.board;
+
+
+      // 번호, 제목, 등록일, 작성자, 조회수, 좋아요
       System.out.printf("%d, %s, %s, %s, %d, %d\n", 
           b.no, 
           b.title, 
@@ -48,15 +58,17 @@ public class BoardHandler {
           b.writer, 
           b.viewCount,
           b.like);
+
+      cursor = cursor.next;
     }
   }
 
   public void detail() {
     System.out.println("[게시글 상세보기]");
-    int no =Prompt.inputInt("번호? ");
+
+    int no = Prompt.inputInt("번호? ");
 
     Board board = findByNo(no);
-
     if (board == null) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
@@ -64,87 +76,118 @@ public class BoardHandler {
 
     board.viewCount++;
     System.out.printf("제목: %s\n", board.title);
-    System.out.printf("내용: %s\n",board.content);
-    System.out.printf("작성자: %s\n" ,board.writer);
+    System.out.printf("내용: %s\n", board.content);
+    System.out.printf("작성자: %s\n", board.writer);
     System.out.printf("등록일: %s\n", board.registeredDate);
-    System.out.printf("조회수: %d\n",board.viewCount);
+    System.out.printf("조회수: %d\n", board.viewCount);
 
   }
 
   public void update() {
-    System.out.println("[게시글 변경하기]");
+    System.out.println("[게시글 변경]");
+
     int no = Prompt.inputInt("번호? ");
 
     Board board = findByNo(no);
-
     if (board == null) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
     }
 
-
     String title = Prompt.inputString(String.format("제목(%s)? ", board.title));
-    String content = Prompt.inputString(String.format("내용(%s)? ",board.content));
+    String content = Prompt.inputString(String.format("내용(%s)? ", board.content));
 
     String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
-    if (input.equalsIgnoreCase("y")) {
+
+    if (input.equalsIgnoreCase("Y")) {
       board.title = title;
       board.content = content;
-      System.out.println("게시글 변경하였습니다.");
+      System.out.println("게시글을 변경하였습니다.");
 
     } else {
       System.out.println("게시글 변경을 취소하였습니다.");
     }
-
-
-
-    System.out.println("해당 번호의 게시글이 없습니다.");
   }
 
   public void delete() {
     System.out.println("[게시글 삭제]");
+
     int no = Prompt.inputInt("번호? ");
 
     Board board = findByNo(no);
-
-    int i = indexOf(no);
-    if (i == -1) {
+    if (board == null) {
       System.out.println("해당 번호의 게시글이 없습니다.");
       return;
-    }       
-    String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+    }
 
-    if (input.equalsIgnoreCase("y")) {
-      for (int x = i + 1; x < size; x++) {
-        boards[x-1] = boards[x];
+    String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
+
+    if (input.equalsIgnoreCase("Y")) {
+      node cursor = head;
+      while (cursor != null) {
+        if (cursor.board == board ) {
+          if (head == lastBox) {
+            head = lastBox = null;
+            break;
+          }
+          if (cursor == head) {
+            head = cursor.next;
+            cursor.prev = null;
+          }else {
+            cursor.prev.next = cursor.next;
+            if (cursor.next != null) {
+              cursor.next .prev = cursor.prev;
+            }
+          }if (cursor == lastBox) {       
+
+            lastBox = cursor.prev;
+          }
+
+
+          break;
+        }
+        cursor = cursor.next;
       }
-      boards[--this.size] = null; // 앞으로 당긴 후 맨 뒤의 항목은 null로 설정한다.
-      System.out.println("게시글을 삭제하였습니다.");      
+
+      System.out.println("게시글을 삭제하였습니다.");
+
     } else {
       System.out.println("게시글 삭제를 취소하였습니다.");
-    }       
-
+    }
 
   }
-
+  // 게시글 번호에 해당하는 인스턴스를 배열에서 찾아 그 인덱스를 리턴한다. 
   int indexOf(int boardNo) {
     for (int i = 0; i < this.size; i++) {
       Board board = this.boards[i];
-      if ( board.no == boardNo) {
+      if (board.no == boardNo) {
         return i;
       }
     }
     return -1;
   }
 
-
-  // 게시글 번호에 해당하는 인스턴스를 찾아 리턴한다
+  // 게시글 번호에 해당하는 인스턴스를 찾아 리턴한다.
   Board findByNo(int boardNo) {
-    int i = indexOf(boardNo);
-    if (i == -1)
-      return null;
-    else
-      return boards[i];
+    node cursor = head;
+    while (cursor != null) {
+      Board b = cursor.board;
+      if (b.no == boardNo) {
+        return b;
+      }
+      cursor = cursor.next;
+    }
+    return null;
+  }
+
+  static class node {
+    Board board;
+    node next;
+    node prev;
+
+    node(Board b) {
+      this.board = b;
+    }
   }
 }
 
